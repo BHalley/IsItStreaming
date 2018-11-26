c4c28f4a8d51d5836cf21ce93e63cb0b644bbe69
Alex Sato <as4628@columbia.edu>
2018-11-26 15:47:35 -0500

    Fixed some bugs that occurred when guidebox couldn't find results. Also searched: updates with search done.

--- assets/logic.js
+++ assets/logic.js
@@ -6,7 +6,6 @@ const GUIDEBOX_URL = "https://api-public.guidebox.com/v2/search?"
 const GUIDEBOX_KEY = "ec97bcc66bfa6f1fc5d73b51e4ca3a5c3c53f058";
 var guideboxID;
 
-//&q=%221roy4o4tqQM%22&key=AIzaSyBCVesxm4Kuui6ID14HPwDSYETR9CJeZ54
 
 $("document").ready(function () {
     $("form").submit(function (event) {
@@ -15,15 +14,15 @@ $("document").ready(function () {
         var searchQuery = $("#mySearch").val();
         window.localStorage.setItem("searchQuery", searchQuery);
         window.location.href = "landing.html";
+
     });
 
     if (localStorage.getItem("searchQuery") != "") {
+        $("#searchHeader").html("<h1>Searched: " + localStorage.getItem("searchQuery") + "</h1>");
         $.ajax({
-            // url: YT_URL + searchQuery + "&key=" + API_KEY,
             url: OMDB_URL + "apikey=" + OMDB_KEY + "&s=" + window.localStorage.getItem("searchQuery"),
             method: "GET"
         }).then(function (response) {
-            console.log(response);
             for (var i = 0; i < 10; i++) {
                 var poster = $("<img>");
                 if (response.Search[i].Poster === "N/A")
@@ -32,7 +31,6 @@ $("document").ready(function () {
                     poster.attr("src", response.Search[i].Poster);
                 poster.attr("alt", response.Search[i].Title);
                 poster.attr("id", response.Search[i].imdbID);
-                console.log(response.Search[i].Type);
                 poster.attr("type", response.Search[i].Type);
                 $("#searchResult").append(poster);
             }
@@ -55,7 +53,6 @@ $("document").ready(function () {
 });
 
 $("body ").on("click", "img", function () {
-    console.log("image clicked!");
 
     var guideboxType = $(this).attr("type"); //At this point, the guidebox type should be "series" or "movie".
     //Other types exist, but we don't care about those.
@@ -65,75 +62,66 @@ $("body ").on("click", "img", function () {
         guideboxType = "show";
         $("#searchResult").html("Retrieving results");
         $.ajax({
-            // url: YT_URL + searchQuery + "&key=" + API_KEY,
             url: GUIDEBOX_URL + "api_key=" + GUIDEBOX_KEY + "&type=show&field=id&id_type=imdb&query=" + this.id,
             method: "GET"
         }).then(function (response) {
-            guideboxID = response.id;
-            console.log("https://api-public.guidebox.com/v2/shows/" + guideboxID + "available_content/?api_key=" + GUIDEBOX_KEY);
-            $.ajax({
-                // url: YT_URL + searchQuery + "&key=" + API_KEY,
-                url: "https://cors-anywhere.herokuapp.com/https://api-public.guidebox.com/v2/shows/" + guideboxID + "/available_content/?api_key=" + GUIDEBOX_KEY,
-                //url: "https://api-public.guidebox.com/v2/movies/" + guideboxID + "?api_key=" + GUIDEBOX_KEY,
-                method: "GET",
-                dataType: "json",
-                // this headers section is necessary for CORS-anywhere
-                headers: {
-                    "x-requested-with": "xhr"
-                }
-            }).then(function (response) {
-                for (var i = 0; i < response.results.android.episodes.all_sources.length; i++)
-                    $("#searchResult").html("<p>Android sources: " + response.results.android.episodes.all_sources[i].display_name+"</p>");
-                for (var i = 0; i < response.results.ios.episodes.all_sources.length; i++)
-                    $("#searchResult").append("<p>ios sources: " + response.results.ios.episodes.all_sources[i].display_name+"</p>");
-                for (var i = 0; i < response.results.web.episodes.all_sources.length; i++)
-                    $("#searchResult").append("<p>Web sources: " + response.results.web.episodes.all_sources[i].display_name+"</p>");
-
-
-
-            });
-
+            if (jQuery.isEmptyObject(response))
+                $("#searchResult").html("No titles found!");
+            else {
+                guideboxID = response.id;
+                $.ajax({
+                    url: "https://cors-anywhere.herokuapp.com/https://api-public.guidebox.com/v2/shows/" + guideboxID + "/available_content/?api_key=" + GUIDEBOX_KEY,
+                    //url: "https://api-public.guidebox.com/v2/movies/" + guideboxID + "?api_key=" + GUIDEBOX_KEY,
+                    method: "GET",
+                    dataType: "json",
+                    // this headers section is necessary for CORS-anywhere
+                    headers: {
+                        "x-requested-with": "xhr"
+                    }
+                }).then(function (response) {
+                    for (var i = 0; i < response.results.android.episodes.all_sources.length; i++)
+                        $("#searchResult").html("<p>Android sources: " + response.results.android.episodes.all_sources[i].display_name + "</p>");
+                    for (var i = 0; i < response.results.ios.episodes.all_sources.length; i++)
+                        $("#searchResult").append("<p>ios sources: " + response.results.ios.episodes.all_sources[i].display_name + "</p>");
+                    for (var i = 0; i < response.results.web.episodes.all_sources.length; i++)
+                        $("#searchResult").append("<p>Web sources: " + response.results.web.episodes.all_sources[i].display_name + "</p>");
+                });
+            }
         });
-
-
-
     }
     else if (guideboxType == "movie") {
         $("#searchResult").html("Retrieving results");
-        console.log(GUIDEBOX_URL + "api_key=" + GUIDEBOX_KEY + "&type=movies&field=id&id_type=imdb&query=" + this.id)
         $.ajax({
-            // url: YT_URL + searchQuery + "&key=" + API_KEY,
             url: GUIDEBOX_URL + "api_key=" + GUIDEBOX_KEY + "&type=movie&field=id&id_type=imdb&query=" + this.id,
             method: "GET"
         }).then(function (response) {
-            guideboxID = response.id;
-            console.log("First response");
-            console.log(response);
-            $.ajax({
-                // url: YT_URL + searchQuery + "&key=" + API_KEY,
-                url: "https://cors-anywhere.herokuapp.com/https://api-public.guidebox.com/v2/movies/" + guideboxID + "?api_key=" + GUIDEBOX_KEY,
-                method: "GET",
-                dataType: "json",
-                // this headers section is necessary for CORS-anywhere
-                headers: {
-                    "x-requested-with": "xhr"
-                }
-            }).then(function (response) {
-                console.log(response);
-
-                for (var i = 0; i < response.purchase_android_sources.length; i++)
-                    $("#searchResult").html("<p>Purchase android sources: " + response.purchase_android_sources[i].display_name + "</p>");
-                for (var i = 0; i < response.purchase_ios_sources.length; i++)
-                    $("#searchResult").append("<p>Purchase ios sources: " + response.purchase_ios_sources[i].display_name + "</p>");
-                for (var i = 0; i < response.purchase_web_sources.length; i++)
-                    $("#searchResult").append("<p>Purchase web sources: " + response.purchase_web_sources[i].display_name + "</p>");
-                for (var i = 0; i < response.subscription_android_sources.length; i++)
-                    $("#searchResult").append("<p>Subscription android sources: " + response.subscription_android_sources[i].display_name + "</p>");
-                for (var i = 0; i < response.subscription_ios_sources.length; i++)
-                    $("#searchResult").append("<p>Subscription ios sources: " + response.subscription_ios_sources[i].display_name + "</p>");
-                for (var i = 0; i < response.subscription_web_sources.length; i++)
-                    $("#searchResult").append("<p>Subscription web sources: " + response.subscription_web_sources[i].display_name + "</p>");
-            });
+            if (jQuery.isEmptyObject(response))
+                $("#searchResult").html("No titles found!");
+            else {
+                guideboxID = response.id;
+                $.ajax({
+                    url: "https://cors-anywhere.herokuapp.com/https://api-public.guidebox.com/v2/movies/" + guideboxID + "?api_key=" + GUIDEBOX_KEY,
+                    method: "GET",
+                    dataType: "json",
+                    // this headers section is necessary for CORS-anywhere
+                    headers: {
+                        "x-requested-with": "xhr"
+                    }
+                }).then(function (response) {
+                    for (var i = 0; i < response.purchase_android_sources.length; i++)
+                        $("#searchResult").html("<p>Purchase android sources: " + response.purchase_android_sources[i].display_name + "</p>");
+                    for (var i = 0; i < response.purchase_ios_sources.length; i++)
+                        $("#searchResult").append("<p>Purchase ios sources: " + response.purchase_ios_sources[i].display_name + "</p>");
+                    for (var i = 0; i < response.purchase_web_sources.length; i++)
+                        $("#searchResult").append("<p>Purchase web sources: " + response.purchase_web_sources[i].display_name + "</p>");
+                    for (var i = 0; i < response.subscription_android_sources.length; i++)
+                        $("#searchResult").append("<p>Subscription android sources: " + response.subscription_android_sources[i].display_name + "</p>");
+                    for (var i = 0; i < response.subscription_ios_sources.length; i++)
+                        $("#searchResult").append("<p>Subscription ios sources: " + response.subscription_ios_sources[i].display_name + "</p>");
+                    for (var i = 0; i < response.subscription_web_sources.length; i++)
+                        $("#searchResult").append("<p>Subscription web sources: " + response.subscription_web_sources[i].display_name + "</p>");
+                });
+            }
 
         });
 
